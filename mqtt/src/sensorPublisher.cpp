1b9e680820ae2e430ff1e764c9126cbb162ac577
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-09-23 15:33:21 -0700

    Create sensorPublisher.cpp

---
+++ mqtt/src/sensorPublisher.cpp
@@ -0,0 +1,15 @@
+#include "sensorPublisher.hpp"
+
+#include <string>
+
+void SensorPublisher::connect()
+{
+}
+
+void SensorPublisher::publishTemperature(double temperature)
+{
+}
+
+void SensorPublisher::publishHumidity(double humidity)
+{
+}
