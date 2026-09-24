763afa6dfdba1039f87454ac19b7fec7c008c36a
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-09-23 18:43:40 -0700

    Update main.cpp

--- MQTT/src/main.cpp
+++ MQTT/src/main.cpp
@@ -1,13 +1,27 @@
 #include "sensorPublisher.hpp"
 
+#include <random>
+#include <chrono>
+#include <thread>
+
 int main()
 {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> tempDist(68.0, 76.0);
+    std::uniform_real_distribution<double> humDist(40.0, 55.0);
+    
     SensorPublisher sensor;
 
     sensor.connect();
 
-    sensor.publishTemperature(72.5);
-    sensor.publishHumidity(46.8);
+    while(1)
+    {
+        sensor.publishTemperature(tempDist(gen));
+        sensor.publishHumidity(humDist(gen));
 
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+    
     return 0;
 }
